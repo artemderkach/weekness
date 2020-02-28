b3cd00894c7e09b5f185446959bdc110f063c14f
mind-rot <artem.derkach.gs@gmail.com>
2020-02-28 18:09:46 +0200

    add postgres package

--- week002_accounts/go.mod
+++ week002_accounts/go.mod
@@ -2,4 +2,9 @@ module github.com/mind-rot/weekness/accounts
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/jackc/pgx/v4 v4.4.1
+	github.com/pkg/errors v0.9.1
+	golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d // indirect
+)
--- week002_accounts/main.go
+++ week002_accounts/main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v4"
+	"github.com/pkg/errors"
 )
 
-type Server struct{}
+type server struct {
+	conn *pgx.Conn
+}
 
 func main() {
-	s := &Server{}
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "error connecting to database"))
+	}
+	defer conn.Close(context.Background())
+
+	s := &server{
+		conn: conn,
+	}
 
 	log.Fatal(http.ListenAndServe(":6969", s.router()))
 }
 
-func (s *Server) router() http.Handler {
+func sendErr(w http.ResponseWriter, msg string) {
+	w.Write([]byte(msg))
+}
+
+func (s *server) router() http.Handler {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", s.handleHome())
@@ -23,8 +42,27 @@ func (s *Server) router() http.Handler {
 	return r
 }
 
-func (s *Server) handleHome() http.HandlerFunc {
+func (s *server) handleHome() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello there!"))
+	}
+}
+
+type Account struct {
+	Name  string
+	Email string
+	Pass  string
+}
+
+func (s *server) handleAccountCreate() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		acc := &Account{}
+
+		err := json.NewDecoder(r.Body).Decode(acc)
+		if err != nil {
+			sendErr(w, "invalid request body")
+			return
+		}
+
 	}
 }
