c2fab0ef9a9b0cd85691e7be1f233d027d12888a
mind-rot <artem.derkach.gs@gmail.com>
2020-03-20 19:16:11 +0200

    change test flow

--- week002_accounts/main_test.go
+++ week002_accounts/main_test.go
@@ -9,7 +9,6 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 
 	"github.com/golang-migrate/migrate/v4"
@@ -20,71 +19,14 @@ import (
 
 var testDatabase = "testdb"
 
-// TestMain prepares database for running tests
-// - create database
-// - run migrations
-// - run tests
-// - close connections and drop database
-func TestMain(m *testing.M) {
-	// create database for test purposes
-	conn, err := pgx.Connect(context.Background(), "postgresql://postgres:pas@localhost:5432/postgres")
-	if err != nil {
-		log.Fatal(errors.Wrap(err, "error connecting to database"))
-	}
+func TestMain(t *testing.T) {
+	err := initDB()
+	require.Nil(t, err)
+	err = dropDB()
+	require.Nil(t, err)
 
-	query := fmt.Sprintf("DROP DATABASE IF EXISTS %s", testDatabase)
-	_, err = conn.Exec(context.Background(), query)
-	if err != nil {
-		log.Fatal(errors.Wrap(err, "error removing test database"))
-	}
-
-	query = fmt.Sprintf("CREATE DATABASE %s", testDatabase)
-	_, err = conn.Exec(context.Background(), query)
-	if err != nil {
-		log.Fatal(errors.Wrap(err, "error creating test database"))
-	}
-
-	// run migration on prepared test database
-	dbURL := fmt.Sprintf("postgres://postgres:pas@localhost:5432/%s?sslmode=disable", testDatabase)
-	migr, err := migrate.New(
-		"file://migrations",
-		dbURL,
-	)
-	if err != nil {
-		log.Fatal(errors.Wrap(err, "error running migrations"))
-	}
-
-	err = migr.Up()
-	if err != nil {
-		log.Fatal(errors.Wrap(err, "cannot run migration"))
-	}
-
-	code := m.Run()
-
-	// clean up connections and db
-	srcErr, dbErr := migr.Close()
-	if srcErr != nil {
-		log.Fatal(errors.Wrap(srcErr, "error closing connection"))
-	}
-	if dbErr != nil {
-		log.Fatal(errors.Wrap(dbErr, "error closing connection"))
-	}
-
-	query = fmt.Sprintf("DROP DATABASE %s", testDatabase)
-	_, err = conn.Exec(context.Background(), query)
-	if err != nil {
-		log.Fatal(errors.Wrap(err, "error dropping test database"))
-	}
-	err = conn.Close(context.Background())
-	if err != nil {
-		log.Fatal(errors.Wrap(err, "error closing connection"))
-	}
-
-	defer os.Exit(code)
-}
-
-func TestM(t *testing.T) {
-	conn, err := pgx.Connect(context.Background(), "postgresql://postgres:pas@localhost:5432/postgres")
+	url := fmt.Sprintf("postgresql://postgres:pas@localhost:5432/%s", testDatabase)
+	conn, err := pgx.Connect(context.Background(), url)
 	require.Nil(t, errors.Wrap(err, "error connecting to db"))
 	defer conn.Close(context.Background())
 
@@ -109,4 +51,85 @@ func TestM(t *testing.T) {
 	body, err := ioutil.ReadAll(res.Body)
 	require.Nil(t, err)
 	require.Equal(t, "created", string(body))
+}
+
+// initDB prepares database for running tests
+// - create test database
+// - run migrations
+// - run sql dump (test data)
+func initDB() error {
+	// create database for test purposes
+	conn, err := pgx.Connect(context.Background(), "postgresql://postgres:pas@localhost:5432/postgres")
+	if err != nil {
+		return errors.Wrap(err, "error connecting to database")
+	}
+
+	defer func() {
+		err = conn.Close(context.Background())
+		if err != nil {
+			log.Println(errors.Wrap(err, "error closing connection"))
+		}
+	}()
+
+	query := fmt.Sprintf("DROP DATABASE IF EXISTS %s", testDatabase)
+	_, err = conn.Exec(context.Background(), query)
+	if err != nil {
+		return errors.Wrap(err, "error removing test database")
+	}
+
+	query = fmt.Sprintf("CREATE DATABASE %s", testDatabase)
+	_, err = conn.Exec(context.Background(), query)
+	if err != nil {
+		return errors.Wrap(err, "error creating test database")
+	}
+
+	// run migration on prepared test database
+	dbURL := fmt.Sprintf("postgres://postgres:pas@localhost:5432/%s?sslmode=disable", testDatabase)
+	migr, err := migrate.New(
+		"file://migrations",
+		dbURL,
+	)
+	if err != nil {
+		return errors.Wrap(err, "error running migrations")
+	}
+
+	defer func() {
+		srcErr, dbErr := migr.Close()
+		if srcErr != nil {
+			log.Println(errors.Wrap(srcErr, "error closing connection"))
+		}
+		if dbErr != nil {
+			log.Println(errors.Wrap(dbErr, "error closing connection"))
+		}
+	}()
+
+	err = migr.Up()
+	if err != nil {
+		return errors.Wrap(err, "cannot run migration")
+	}
+
+	return nil
+}
+
+func dropDB() error {
+	url := fmt.Sprintf("postgresql://postgres:pas@localhost:5432/%s", testDatabase)
+	conn, err := pgx.Connect(context.Background())
+	if err != nil {
+		return errors.Wrap(err, "error connecting to database")
+	}
+
+	defer func() {
+		err = conn.Close(context.Background())
+		if err != nil {
+			log.Println(errors.Wrap(err, "error closing connection"))
+		}
+	}()
+
+	query = fmt.Sprintf("DROP DATABASE %s", testDatabase)
+	_, err = conn.Exec(context.Background(), query)
+	if err != nil {
+		return errors.Wrap(err, "error dropping test database")
+	}
+
+	return nil
 }
